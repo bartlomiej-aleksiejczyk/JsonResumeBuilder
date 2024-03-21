cbcdfe304dbd48b67bb27ef462f9695cd9a39ab9
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-03-21 22:05:53 +0100

    Update model with compilation result

--- client/src/main/java/com/example/jsonresumebuilderspring/JsonresumebuilderspringApplication.java
+++ client/src/main/java/com/example/jsonresumebuilderspring/JsonresumebuilderspringApplication.java
@@ -1,7 +1,9 @@
 package com.example.jsonresumebuilderspring;
 
+import org.hibernate.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JsonresumebuilderspringApplication {
@@ -9,5 +11,6 @@ public class JsonresumebuilderspringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JsonresumebuilderspringApplication.class, args);
 	}
+
 
 }
--- client/src/main/java/com/example/jsonresumebuilderspring/cvbuildscheduler/CvBuildJob.java
+++ client/src/main/java/com/example/jsonresumebuilderspring/cvbuildscheduler/CvBuildJob.java
@@ -1,19 +1,31 @@
 package com.example.jsonresumebuilderspring.cvbuildscheduler;
 
 import com.example.jsonresumebuilderspring.common.BaseEntity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
-@AllArgsConstructor
+
 @Getter
 @Setter
+@Entity
+@Table(name = "cv_build_job")
+
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class CvBuildJob extends BaseEntity {
     private String jsonContent;
     private String templateName;
 
     @Enumerated(EnumType.STRING)
     private JobStatus status;
+
+    @Lob
+    byte[] cvCompilationResult;
+
+    public CvBuildJob(String jsonContent, String templateName, JobStatus status) {
+        this.jsonContent = jsonContent;
+        this.templateName = templateName;
+        this.status = status;
+    }
 }
