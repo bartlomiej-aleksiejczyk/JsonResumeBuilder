4137faf75af1306bee3baef806447febf0098c99
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-03-21 21:29:30 +0100

    Update liquibase migrations with sql and config

---
+++ client/src/main/resources/db/changelog/changes/0001_initial_schema.sql
@@ -0,0 +1,17 @@
+--liquibase formatted sql
+
+--changeset bartlomiej_aleksiejczyk:0001-01
+CREATE SEQUENCE id_seq START WITH 1 INCREMENT BY 1;
+
+--changeset bartlomiej_aleksiejczyk:0001-02
+CREATE TABLE cv_build_job (
+    id BIGINT DEFAULT nextval('id_seq') PRIMARY KEY,
+    json_content TEXT NOT NULL,
+    template_name VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    modified_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+--rollback DROP TABLE cv_build_job;
+--rollback DROP SEQUENCE id_seq;
