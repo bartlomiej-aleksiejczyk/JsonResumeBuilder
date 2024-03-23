66d3c7b7aadfefb9d27d4ef512c9f657e5baf48b
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-03-23 17:47:42 +0100

    Add missing import

--- client/src/main/java/com/example/jsonresumebuilderspring/JsonresumebuilderspringApplication.java
+++ client/src/main/java/com/example/jsonresumebuilderspring/JsonresumebuilderspringApplication.java
@@ -4,6 +4,7 @@ import org.hibernate.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
