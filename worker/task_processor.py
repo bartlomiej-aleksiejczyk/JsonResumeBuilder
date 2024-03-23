c4ed184d131cf5bd08de7998351a0b785745fd6d
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-03-23 19:48:29 +0100

    Update docker config of python worker

--- worker/task_processor.py
+++ worker/task_processor.py
@@ -1,16 +1,26 @@
 import os
-import logging
 from celery import Celery
+import logging
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
-app = Celery('message_listener',
-             broker=os.environ.get('CELERY_BROKER_URL'))
+app = Celery('worker', broker=os.environ.get('CELERY_BROKER_URL'))
 
-@app.task(bind=True, queue='resumebuilder')
-def listen_and_log(self, message):
-    logger.info(f"Received message: {message}")
+app.conf.task_queues = {
+    os.environ.get('CELERY_QUEUE', 'default'): {
+        'exchange': os.environ.get('CELERY_QUEUE', 'default'),
+        'routing_key': os.environ.get('CELERY_QUEUE', 'default'),
+    },
+}
+
+@app.task(bind=True)
+def log_message(self, message):
+    logging.info(f"Received message: {message}")
 
 if __name__ == '__main__':
-    app.worker_main()
+    app.worker_main(argv=[
+        'worker',
+        '--loglevel=INFO',
+        '--concurrency=4',
+        '-Q', os.environ.get('CELERY_QUEUE', 'default'),
+    ])
