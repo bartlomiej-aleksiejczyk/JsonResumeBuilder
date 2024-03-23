a9b42ded690179e38f4f1dacf68e1c84c8b6f446
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-03-23 09:40:37 +0100

    Remove old convention in spring SecurityConfig

--- client/src/main/java/com/example/jsonresumebuilderspring/common/configuration/SecurityConfig.java
+++ client/src/main/java/com/example/jsonresumebuilderspring/common/configuration/SecurityConfig.java
@@ -5,13 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
