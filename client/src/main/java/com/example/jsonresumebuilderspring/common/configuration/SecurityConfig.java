012c9cd2957b133e3efdac72c660cb0eae4c924d
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-03-23 16:18:22 +0100

    Set up test spring security

--- client/src/main/java/com/example/jsonresumebuilderspring/common/configuration/SecurityConfig.java
+++ client/src/main/java/com/example/jsonresumebuilderspring/common/configuration/SecurityConfig.java
@@ -2,36 +2,42 @@ package com.example.jsonresumebuilderspring.common.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic(withDefaults())
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
             .authorizeHttpRequests((requests) -> requests
                     .anyRequest().authenticated()
-            );
+            ).httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("user")
-                .password(passwordEncoder().encode("password"))
-                .authorities("ROLE_USER");
+    @Bean
+    public UserDetailsService userDetailsService() {
+        SCryptPasswordEncoder encoder = SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8();
+        String result = encoder.encode("password");
+        System.out.println(result);
+        UserDetails user =
+                User.withUsername("username")
+                        .password(result)
+                        .roles("USER")
+                        .build();
+        return new InMemoryUserDetailsManager(user);
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new SCryptPasswordEncoder();
-    }
 }
