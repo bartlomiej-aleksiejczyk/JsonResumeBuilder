0340e54eb32181662a06497f5487fce84dc71944
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-03-24 15:21:33 +0100

    Add template model to Spring Boot app

---
+++ client/src/main/java/com/example/jsonresumebuilderspring/cvlatextemplate/CvLatexTemplate.java
@@ -0,0 +1,23 @@
+package com.example.jsonresumebuilderspring.cvlatextemplate;
+
+import com.example.jsonresumebuilderspring.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "cv_latex_template")
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CvLatexTemplate extends BaseEntity {
+    private String templateContent;
+
+    @Column(nullable = false)
+    private boolean deleted = false;
+
+    public CvLatexTemplate(String templateContent) {
+        this.templateContent = templateContent;
+    }
+}
