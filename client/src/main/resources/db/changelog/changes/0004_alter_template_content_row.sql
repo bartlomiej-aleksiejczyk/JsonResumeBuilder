44646e6339981f91977fc047e23d836ff51d2236
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-03-25 21:57:29 +0100

    Fix migrations in Spring Boot app

--- client/src/main/resources/db/changelog/changes/0004_alter_template_content_row.sql
+++
@@ -1,4 +0,0 @@
---changeset bartlomiej_aleksiejczyk:0004-01
-ALTER TABLE cv_latex_template ALTER COLUMN template_content TYPE TEXT;
-
---rollback ALTER TABLE cv_latex_template ALTER COLUMN template_content TYPE VARCHAR(255);
