91fe667e2286157b846726b61bbeab8b06e9bad2
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-03-26 21:02:31 +0100

    Fix initial migration

--- client/src/main/resources/db/changelog/changes/0001_initial_schema.sql
+++ client/src/main/resources/db/changelog/changes/0001_initial_schema.sql
@@ -10,6 +10,7 @@ CREATE TABLE cv_build_job (
     template_id BIGINT,
     json_content TEXT NOT NULL,
     status VARCHAR(255) NOT NULL,
+    cv_compilation_result BYTEA,
     created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
     modified_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
