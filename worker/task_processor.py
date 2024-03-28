4a01985e905d3532838665ceaabf63441a833862
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-03-28 19:52:57 +0100

    Add error handling to worker to fail task

--- worker/task_processor.py
+++ worker/task_processor.py
@@ -17,8 +17,11 @@ app.conf.task_queues = {
     },
 }
 
+//TODO: Improve error handling i want everything worng to fail task
 @app.task(bind=True, name=os.environ.get('QUEUE_CELERY_TASK_BUILD_CV'))
 def process_message_and_compile_latex(self, message):
+    try:
+
     logging.info(f"Received message: {message}")
 
     message_id = message.get('id', '')
@@ -28,7 +31,10 @@ def process_message_and_compile_latex(self, message):
     output_tex_filename = f'filled_template_{message_id}.tex'
     output_pdf_filename = f'filled_template_{message_id}.pdf'
 
-    try:
+    username = os.environ.get('SPRING_SINGLE_LOGIN')
+    password = os.environ.get('SPRING_SINGLE_PASSWORD')
+    url = f"http://springboot-server:8080/{os.environ.get('QUEUE_EXCHANGE')}/api/v1/cv-build-job/{message_id}/status"
+    
         env = Environment(
             loader=FileSystemLoader('.'),
             block_start_string='\BLOCK{',
@@ -56,11 +62,9 @@ def process_message_and_compile_latex(self, message):
 
     except Exception as e:
         logging.error(f"Error processing template: {e}")
-        return
+        return report_task_failure(url, username, password)
 
-    username = os.environ.get('SPRING_SINGLE_LOGIN')
-    password = os.environ.get('SPRING_SINGLE_PASSWORD')
-    url = f"http://springboot-server:8080/{os.environ.get('QUEUE_EXCHANGE')}/api/v1/cv-build-job/{message_id}/status"
+  
     status_update = json.dumps({'status': 'IN_PROGRESS'})
 
     files = {
@@ -76,6 +80,19 @@ def process_message_and_compile_latex(self, message):
     finally:
         os.remove(output_tex_filename)
         os.remove(output_pdf_filename)
+
+def report_task_failure(url, username, password):
+    status_update_failed = json.dumps({'status': 'FAILED'})
+        
+    files_failed = {
+        'statusUpdate': ('', status_update_failed, 'application/json'),
+        'file': ('', '', 'application/octet-stream')
+    }
+    try:
+            response = requests.patch(url, files=files_failed, auth=HTTPBasicAuth(username, password))
+            logging.info(f"PATCH request with failure status to {url} returned: {response.status_code}")
+    except Exception as e:
+            logging.error(f"Error sending failed status update: {e}")
 
 if __name__ == '__main__':
     app.worker_main(argv=[
