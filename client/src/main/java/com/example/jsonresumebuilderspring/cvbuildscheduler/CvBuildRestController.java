382f32c9edd209512f17e0f089a924051cbe6075
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-04-12 18:25:42 +0200

    Add comment and logging for test purposes

--- client/src/main/java/com/example/jsonresumebuilderspring/cvbuildscheduler/CvBuildRestController.java
+++ client/src/main/java/com/example/jsonresumebuilderspring/cvbuildscheduler/CvBuildRestController.java
@@ -24,6 +24,7 @@ public class CvBuildRestController {
 
         CvBuildJob updatedJob;
         if (compiledCvFile != null && !compiledCvFile.isEmpty()) {
+            System.out.println(compiledCvFile.getName());
             updatedJob = cvBuildJobService.updateBuildJobStatus(id, statusUpdate.getStatus(), compiledCvFile);
         } else {
             updatedJob = cvBuildJobService.updateBuildJobStatus(id, statusUpdate.getStatus(), null);
